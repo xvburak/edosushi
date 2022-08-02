133e027f8c59178b4af03cdbbed17725bc3b2ea6
xvburak <xvburak@vutbr.cz>
2022-08-02 17:22:31 +0200

    checkout

--- src/routes/cart3.svelte
+++ src/routes/cart3.svelte
@@ -1,11 +1,22 @@
-<div class="mx-auto text-left text-4xl max-w-screen-md flex items-center">
-    <div class="tenor-gif-embed mix-blend-multiply h-24" 
-    data-postid="16677782" 
-    data-share-method="host" 
-    data-aspect-ratio="1.02564"
-    data-width="100%">
-    <a href="./static/icons-ok.gif">Check GIFs</a>
-</div>
+<script>
+    import {
+        onMount
+    } from 'svelte';
+
+    onMount(() => {
+        setTimeout(function () {
+            window.location.href = "/";
+        }, 5000);
+    });
+</script>
+
+<div class="mx-auto text-left text-4xl max-w-screen-md flex space-x-4 items-center text-white">
+
+    <div class="mix-blend-multiply">
+        <img class="" src="/icons8-checkmark.gif" width="200" alt="404 image" />
+    </div>
+
+
 
     <p>Vaše objednávka byla úspěšně přijata,
         arigatōgozaimashita!</p>
