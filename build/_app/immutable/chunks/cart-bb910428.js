c2a819ffb56e3e192a8abd296e7a4d41cf0420b7
xvburak <xvburak@vutbr.cz>
2022-08-09 15:32:14 +0200

    j

---
+++ build/_app/immutable/chunks/cart-bb910428.js
@@ -0,0 +1,1 @@
+import{r as t,w as i}from"./index-3d151600.js";const e=t([{id:1,title:"Family Set",q:43,price:860,quantity:1,contents:[{title:"hoso maki",q:18},{title:"uramaki a futomaki",q:16},{title:"nigiri",q:9}]},{id:2,q:69,title:"Party Set",price:1360,quantity:1,contents:[{title:"hoso maki",q:30},{title:"uramaki a futomaki",q:24},{title:"nigiri",q:15}]}]),r=i([]);export{r as c,e as p};
