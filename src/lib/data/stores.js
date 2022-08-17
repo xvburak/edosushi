d448b69f8b83c0cb79a894ceaaf495f348547428
xvburak <xvburak@vutbr.cz>
2022-08-17 13:25:52 +0200

    Update stores.js

--- src/lib/data/stores.js
+++ src/lib/data/stores.js
@@ -1,10 +1,4 @@
-import { writable, derived, readable } from 'svelte/store';
-
-export const itemsInCart = writable(0);
-export const cartContents = writable([]);
-export const totalPrice = derived(itemsInCart, $itemsInCart => $itemsInCart * .5);
-
-export const fullCount = writable(0);
+import { writable, readable } from 'svelte/store';
 
 export const dage = writable([]);
 export const methods = readable(['Hotovost na místě', 'Platba kartou na místě', 'Platba převodem'])
