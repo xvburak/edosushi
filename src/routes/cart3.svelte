544015828ba515b460171c819456136e6a9f7ec5
xvburak <xvburak@vutbr.cz>
2022-10-17 17:00:48 +0200

    Update cart3.svelte

--- src/routes/cart3.svelte
+++ src/routes/cart3.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
     import {
         onMount
     } from 'svelte';
@@ -8,15 +8,13 @@
             window.location.href = "/";
         }, 10000);
     });
-</script>
+</script> -->
 
 <div class="mx-auto text-left text-4xl max-w-screen-lg flex space-x-4 items-center text-white">
 
     <div class="mix-blend-multiply">
         <img  src="/icons8-checkmark.gif" width="200" alt="checkout info" />
     </div>
-
-
 
     <p>Vaše objednávka byla úspěšně přijata, arigatōgozaimashita!</p>
 </div>
